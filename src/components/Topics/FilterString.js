f8ec0e14bc64a4cc81facf6584792fe8820885c5
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro.local>
2019-04-29 21:24:39 -0600

    Update v2

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react'
 
 export default class FilterString extends Component {
-    constructor(){
+    constructor() {
         super();
+
         this.state = {
-            unfilteredArray: [''],
+            names: ['James', 'Jimmy', 'Michelle', 'Olivia', 'Samantha', 'Jennifer', 'Michael', 'Thomas'],
             userInput: '',
-            filteredArray: ''
-        }
+            filteredNames: []
+        };
     }
+    handleChange(val) {
+        this.setState({ userInput: val });
+    }
+
+    filterNames(userInput) {
+        let names = this.state.names;
+        let filteredNames = [];
+
+        for (let i = 0; i < names.length; i++) {
+            if (names[i].includes(userInput)) {
+                filteredNames.push(names[i]);
+            }
+        }
+        this.setState({ filteredNames: filteredNames });
+    }
+
     render() {
-        return (
-            <div className="puzzleBox filterStringPB">
-                <h4>Filter String</h4>
-                <span className="puzzleText"></span>
-                <input className="inputLine"></input>
-                <button className="confirmationButton"></button>
-                <span className="resultsBox filterStringRB"></span>
-            </div>
+            return (
+                <div className="puzzleBox filterStringPB">
+                    <h4> Filter String </h4>
+                    <span className="puzzleText"> Names: {JSON.stringify(this.state.names, null, 10)} </span>
+                    <input className="inputLine" onChange={(e) => this.handleChange(e.target.value)}></input>
+                    <button className="confirmationButton" onClick={() => this.filterNames(this.state.userInput)}> Filter </button>
+                    <span className="resultsBox filterStringRB"> Filtered Names: {JSON.stringify(this.state.filteredNames, null, 10)} </span>
+                </div>
         )
     }
-}
+ }
